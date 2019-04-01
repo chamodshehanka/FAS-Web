53e2a97359598db9f2de19ccf1a7cb76ffdbac9d
Shehanka <hcsperera@outlook.com>
2019-04-01 16:06:24 +0530

    home and carousel created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CarouselComponent } from './home/carousel/carousel.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    CarouselComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
